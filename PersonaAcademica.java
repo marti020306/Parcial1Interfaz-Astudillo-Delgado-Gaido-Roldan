d283eab1faf4d97e9098e2f7aed175ff6c6b2278
Martina Roldan <laramartinaroldan@gmail.com>
2026-05-04 19:30:45 -0300

    Actualice Trabajo

---
+++ PersonaAcademica.java
@@ -0,0 +1,43 @@
+public class PersonaAcademica 
+{
+    private String nombre;
+    private String legajo;
+
+    public PersonaAcademica(String nombre, String legajo) 
+    {
+        setNombre(nombre);
+        setLegajo(legajo);
+    }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        if (nombre == null || nombre.isEmpty()) 
+        {
+            throw new IllegalArgumentException("Nombre invalido");
+        }
+        this.nombre = nombre;
+    }
+
+    public String getLegajo()
+    {
+        return legajo;
+    }
+
+    public void setLegajo(String legajo)
+    {
+        if (legajo == null)
+        {
+            throw new IllegalArgumentException("Legajo invalido");
+        }
+
+        this.legajo = legajo;
+    }
+
+    public abstract void MostrarResumen();
+
+}
