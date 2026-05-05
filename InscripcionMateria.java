52c1c8d60f37a9e4c5547fe428dd4fd5c13cdb24
guada astudillo <astudillloguadalupe@gmail.com>
2026-05-04 23:54:09 -0300

    feat: cree estructura de InscripcionMateria

---
+++ InscripcionMateria.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class InscripcionMateria implements Evaluable 
+{
+
+    private Materia materia;
+    private int totalClases;
+    private int clasesAsistidas;
+    private ArrayList<Double> notas;
+
+    private static final int MAX_NOTAS = 5;
+    private static final double MINIMO_REGULAR = 75.0;
+
+    public InscripcionMateria(Materia materia) 
+    {
+        this.materia = materia;
+        this.totalClases = 0;
+        this.clasesAsistidas = 0;
+        this.notas = new ArrayList<Double>();
+    }
+}
