de84d3387657f226d9dc8d855ba2f70db5276baf
delgadosofiaguada-jpg <delgado.sofiaguada@gmail.com>
2026-05-06 19:20:08 -0300

    Clase Estudiante

--- Estudiante.java
+++ Estudiante.java
@@ -1,3 +1,10 @@
-public class Estudiante
-{ }
+import java.util.ArrayList;
+import java.util.Scanner;
+package academico;
+public class Estudiante extends PersonaAcademica implements Consultable
+{
+    private String carrera;
+    private int anioIngreso;
+    private ArrayList<InscripcionMateria>materias;
+}
     
