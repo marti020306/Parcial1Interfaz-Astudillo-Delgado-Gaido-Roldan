bf71775c7a9b02dbd2b70472b556edcc555615a3
delgadosofiaguada-jpg <delgado.sofiaguada@gmail.com>
2026-05-07 13:29:34 -0300

    Validar y crear GetPromedio

--- Estudiante.java
+++ Estudiante.java
@@ -60,3 +60,29 @@ public void darDeBaja(String codigoMateria)
 {
     materias.removeIf(i->i.getMateria().getCodigo().equalsIgnoreCase(codigoMateria))
 }
+public InscripcionMateria getInscripcion(String codigoMateria)
+{
+    for (InscripcionMateria i : materias)
+    {
+        if (i.getMateria().getCodigo().equalsIgnoreCase(codigoMateria))
+        {
+            return i;
+        }
+    }
+    return null;
+}
+
+//Promedio general
+public double getPromedioGeneral()
+{
+    if (materias.isEmpty())
+    {
+        return o;
+    }
+    double suma=0;
+    for (InscripcionMateria i : materias)
+    {
+        suma=suma+i.getPromedio();
+    }
+    return suma/materias.size();
+}
